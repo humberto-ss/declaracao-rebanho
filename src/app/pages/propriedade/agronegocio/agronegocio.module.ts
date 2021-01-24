d6d84360a9794997e8c722a3ed8e9725f629f563
Humberto <humberto-santanna@procergs.rs.gov.br>
2021-01-24 19:59:14 -0300

    Update agronegocios page
    Include segments: lancamentos, declaracoes.
    When show the components

--- src/app/pages/propriedade/agronegocio/agronegocio.module.ts
+++ src/app/pages/propriedade/agronegocio/agronegocio.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { AgronegocioPageRoutingModule } from './agronegocio-routing.module';
 
 import { AgronegocioPage } from './agronegocio.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AgronegocioPageRoutingModule
+    AgronegocioPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [AgronegocioPage]
 })
--- src/app/pages/propriedade/agronegocio/agronegocio.page.ts
+++ src/app/pages/propriedade/agronegocio/agronegocio.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { AgronegocioModel } from 'src/app/components/agronegocios/agronegocio.model';
+import { AgronegocioService } from 'src/app/services/agronegocio.service';
 
 @Component({
   selector: 'app-agronegocio',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agronegocio.page.scss'],
 })
 export class AgronegocioPage implements OnInit {
-
-  constructor() { }
+  agronegocio: AgronegocioModel;
+  blnShowLancamento: boolean = false;
+  blnShowDeclaracao: boolean = false;
+  constructor(
+    private actvRouter: ActivatedRoute,
+    private AgronegocioService: AgronegocioService,
+    private navController: NavController
+  ) { }
 
   ngOnInit() {
+    this.actvRouter.paramMap.subscribe(
+      paramMap =>{
+        if(!paramMap.has('agronegocioId')){
+           this.navController.navigateBack('propriedade');
+           return;
+        }
+        this.agronegocio = this.AgronegocioService.getAgronegociosPorId(+paramMap.get('agronegocioId'))
+
+        //Se nao localizou o agronegocio, retorna
+        if(!this.agronegocio){
+          this.navController.navigateBack('propriedade');
+          return;
+        }
+      });
+      this.blnShowLancamento = true;
   }
+  segmentChanged(ev: any){
+    var blnSegment: boolean = true;
+     (ev.target.value ==='lancamentos')? blnSegment = true: blnSegment=false;
+      this.blnShowLancamento = blnSegment;
+      this.blnShowDeclaracao = !blnSegment;  
+  }
+
 
 }
