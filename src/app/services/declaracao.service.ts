fe0ee60b98a9e51ec2af6fd9624b7bc93ce8afe1
Humberto <humberto-santanna@procergs.rs.gov.br>
2021-01-30 17:04:38 -0300

    Popula Components
    Popula os dados dos components Tipo Exploracao e Declaracoes

--- src/app/services/declaracao.service.ts
+++ src/app/services/declaracao.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from "@angular/core";
+import { AgronegocioModel } from "../components/agronegocios/agronegocio.model";
+import { TipoExploracaoModel } from "../components/tipos-exploracao/tipo-exploracao.model";
+import { AgronegocioService } from "./agronegocio.service";
+import { EspecieAnimalEnum } from  '../util/especieAnimalEnum'
+import { RacaEnum } from "../util/racaEnum";
+import { PopulaDados } from "./populaDados"
+import { DeclaracaoModel } from "../components/declaracoes/declaracao.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class DeclaracaoService {
+export class DeclaracaoService implements OnInit {
+  private _tipoExploracaoPopulado: TipoExploracaoModel[];
+  private _tipoExploracao: TipoExploracaoModel[]=[];
+  private tipoExploracaoSelecionado: TipoExploracaoModel;
 
-  constructor() { }
+  agronegocioModel: AgronegocioModel;
+  constructor(private agronegocioService: AgronegocioService) {
+    this.populaTipoExploracao();
+    this.agronegocioModel = this.agronegocioService.agronegocioSelecionado;
+    this.tipoExploracaoPorEspecieAnimal();
+  }
+
+  ngOnInit() {
+  }
+
+  get tipoExploracao() {
+      return this._tipoExploracao;
+  }
+
+  tipoExploracaoPorEspecieAnimal(){
+    LOOP_TIPO:
+    for(let tipo of this._tipoExploracaoPopulado){
+      LOOP_ESPECIE:
+      for(let especieID of tipo.idEspecieAnimal){
+        if(especieID === this.agronegocioModel.idEspecieAnimal){
+          this._tipoExploracao.push(tipo);
+          continue LOOP_TIPO;
+        }
+      }
+    }
+  }
+
+  populaDeclaracoesComponent(){
+    return new PopulaDados().populaDeclaracoesComponent();
+  }
+
+  populaTipoExploracao() {
+    return this._tipoExploracaoPopulado = new PopulaDados().populaTipoExploracao();
+  }
 }
