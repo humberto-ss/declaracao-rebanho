7ffac9adb4e5bf481b05aeb2cd39d46cbfdbdd05
Humberto <humberto-santanna@procergs.rs.gov.br>
2021-01-30 17:05:08 -0300

    Data Atual formato BR

---
+++ src/app/util/util.ts
@@ -0,0 +1,11 @@
+export class Util{
+    constructor(){}
+    
+    getDate(){
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        return `${dd}/${mm}/${yyyy}`;
+    }
+}
