f2e092f318d7bd29b26b4dfb92d6e03ff687d40a
Humberto <humberto-santanna@procergs.rs.gov.br>
2021-01-30 18:34:05 -0300

    Add components into components.module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,6 +7,8 @@ import { AgronegociosComponent } from "./agronegocios/agronegocios.component";
 import { RouterModule } from "@angular/router";
 import { LancamentosComponent } from "./lancamentos/lancamentos.component";
 import { DeclaracoesComponent } from "./declaracoes/declaracoes.component";
+import { TiposLancamentosComponent } from "./tipos-lancamentos/tipos-lancamentos.component";
+import { TiposExploracaoComponent } from "./tipos-exploracao/tipos-exploracao.component";
 
 @NgModule({
   imports: [IonicModule, FormsModule, CommonModule, RouterModule],
@@ -14,13 +16,17 @@ import { DeclaracoesComponent } from "./declaracoes/declaracoes.component";
     PropriedadesRuraisComponent,
     AgronegociosComponent,
     LancamentosComponent,
-    DeclaracoesComponent
+    DeclaracoesComponent,
+    TiposLancamentosComponent,
+    TiposExploracaoComponent
   ],
   exports: [
     PropriedadesRuraisComponent,
     AgronegociosComponent,
     LancamentosComponent,
-    DeclaracoesComponent
+    DeclaracoesComponent,
+    TiposLancamentosComponent,
+    TiposExploracaoComponent
   ],
 })
 export class ComponentsModule {}
