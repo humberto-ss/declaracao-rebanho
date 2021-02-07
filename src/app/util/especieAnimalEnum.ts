3ad58d30fcebe4ae3cddd9eef8fa2d5eb9c59808
Humberto <humberto-santanna@procergs.rs.gov.br>
2021-02-07 12:46:04 -0300

    Corrigido Enum Especie Animal

--- src/app/util/especieAnimalEnum.ts
+++ src/app/util/especieAnimalEnum.ts
@@ -4,27 +4,27 @@ import { RacaEnum } from './racaEnum'
 export class EspecieAnimalEnum{
     public static bovinos: EspecieAnimalDTO = {id: 0, nome:"Bovinos"};
     public static bubalinos: EspecieAnimalDTO = {id: 1, nome:"Bubalinos"};
-    public static ovinos: EspecieAnimalDTO = {id: 1, nome:"Ovinos"};
-    public static caprinos: EspecieAnimalDTO = {id: 1, nome:"Caprinos"};
-    public static suinos: EspecieAnimalDTO = {id: 1, nome:"Suinos"};
-    public static javalis: EspecieAnimalDTO = {id: 1, nome:"Javalis"};
-    public static equideos: EspecieAnimalDTO = {id: 1, nome:"Equideos",
+    public static ovinos: EspecieAnimalDTO = {id: 2, nome:"Ovinos"};
+    public static caprinos: EspecieAnimalDTO = {id: 3, nome:"Caprinos"};
+    public static suinos: EspecieAnimalDTO = {id: 4, nome:"Suinos"};
+    public static javalis: EspecieAnimalDTO = {id: 5, nome:"Javalis"};
+    public static equideos: EspecieAnimalDTO = {id: 6, nome:"Equideos",
      raca:[RacaEnum.Equinos,RacaEnum.Muares, RacaEnum.Asininos]};
 
-    public static Animais_Aquaticos: EspecieAnimalDTO = {id: 1, nome:"Animais Aquaticos",
+    public static Animais_Aquaticos: EspecieAnimalDTO = {id: 7, nome:"Animais Aquaticos",
     raca:[RacaEnum.Peixes, RacaEnum.Camarao, RacaEnum.Moluscos]};
 
-    public static abelhas: EspecieAnimalDTO = {id: 1, nome:"Abelhas",
+    public static abelhas: EspecieAnimalDTO = {id: 8, nome:"Abelhas",
      raca:[RacaEnum.Melifera, RacaEnum.Meliponinia]};
 
-    public static aves: EspecieAnimalDTO = {id: 1, nome:"Aves",
+    public static aves: EspecieAnimalDTO = {id: 9, nome:"Aves",
     raca:[RacaEnum.Galinhas, RacaEnum.Codornas, RacaEnum.Perus, RacaEnum.Patos,
          RacaEnum.Marrecos,RacaEnum.Gansos, RacaEnum.Faisoes, RacaEnum.Galinhas_angola,
          RacaEnum.Outras_Aves]}
 
-    public static caes: EspecieAnimalDTO = {id: 1, nome:"Cães"};
-    public static gatos: EspecieAnimalDTO = {id: 1, nome:"Gatos"};
-    public static coelhos: EspecieAnimalDTO = {id: 1, nome:"Coelhos"};
+    public static caes: EspecieAnimalDTO = {id: 10, nome:"Cães"};
+    public static gatos: EspecieAnimalDTO = {id: 11, nome:"Gatos"};
+    public static coelhos: EspecieAnimalDTO = {id: 12, nome:"Coelhos"};
     
     public values =[
         EspecieAnimalEnum.bovinos,
