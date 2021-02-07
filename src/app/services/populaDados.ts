335d5af6ba17a89008dca56da2ec81232663d751
Humberto <humberto-santanna@procergs.rs.gov.br>
2021-02-07 12:45:33 -0300

    Update populaDados.ts

--- src/app/services/populaDados.ts
+++ src/app/services/populaDados.ts
@@ -1,14 +1,22 @@
-import { AgronegocioModel } from "../components/agronegocios/agronegocio.model";
 import { DeclaracaoModel } from "../components/declaracoes/declaracao.model";
-import { PropriedadesRuraisModel } from "../components/propriedades-rurais/propriedades-rurais.model";
 import { TipoExploracaoModel } from "../components/tipos-exploracao/tipo-exploracao.model";
 import { CategoriasEnum } from "../util/categoriasEnum";
 import { EspecieAnimalEnum } from "../util/especieAnimalEnum";
 import { RacaEnum } from "../util/racaEnum";
 import { SubCategoriaEnum } from "../util/subCategoriaEnum";
+import { PropriedadeRuralDTO } from "../DTOs/propriedadeRuralDTO";
+import { EspecieAnimalDTO} from '../DTOs/especieAnimalDTO'
+import { AgronegocioDTO} from '../DTOs/agronegocioDTO';
+import { TiposLancamentosModel } from "../components/tipos-lancamentos/tipos-lancamentos.model";
 
 export class PopulaDados {
-  constructor() {}
+  private propriedadesRurais: PropriedadeRuralDTO[];
+  private tiposLancamento: TiposLancamentosModel[]=[];
+  constructor() {
+    this.propriedadesRurais = this.populaPropriedadesDTO();
+    this.tiposLancamento = this.populaTiposLancamentos();
+
+  }
 
   populaDeclaracoesComponent() {
     return [
@@ -43,140 +51,152 @@ export class PopulaDados {
           quantidade: 20,
         },
       ]),
+      new DeclaracaoModel(4, 3, "02/02/2020", 9, true, '02/02/2020', [
+        {
+          idSubAgrupamento: SubCategoriaEnum.macho.id,
+          idAgrupamento: CategoriasEnum.de_25_a_36_meses.id,
+          quantidade: 3,
+        },
+        {
+          idSubAgrupamento: SubCategoriaEnum.femea.id,
+          idAgrupamento: CategoriasEnum.de_25_a_36_meses.id,
+          quantidade: 6,
+        },
+      ]),
     ];
   }
 
-  populaPropriedadesComponent() {
+  populaPropriedadesDTO() {
     return [
-      new PropriedadesRuraisModel(0, "Estrela da Morte", 20, "RS1241412BR"),
-      new PropriedadesRuraisModel(1, "Vulcano", 50, "RS000000BR"),
-      new PropriedadesRuraisModel(2, "Asgard", 88, "RS999999BR"),
-      new PropriedadesRuraisModel(3, "Gothan", 99, "RS984230958234BR"),
+      {id:0, nome: "Estrela da Morte", area: 20, inscricaoEstadual: "RS1241412BR"},
+      {id:1, nome: "Vulcano", area: 50, inscricaoEstadual: "RS000000BR"},
+      {id:2, nome: "Asgard", area: 20, inscricaoEstadual: "RS999999BR"},
+      {id:3, nome: "Gothan", area: 99, inscricaoEstadual: "RS984230958234BR"}
     ];
   }
 
   populaAgronegocios() {
-    return [
-      new AgronegocioModel(
-        0,
-        0,
-        EspecieAnimalEnum.bovinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bovinos.nome],
-        "LUKE SKYWALKER, ANAKIN SKYWALKER",
-        true
-      ),
-      new AgronegocioModel(
-        1,
-        0,
-        EspecieAnimalEnum.bubalinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bubalinos.nome],
-        "LUKE SKYWALKER",
-        true
-      ),
-      new AgronegocioModel(
-        2,
-        0,
-        EspecieAnimalEnum.caprinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.caprinos.nome],
-        "LUKE SKYWALKER, PRINCESS LEIA ORGANA SKYWALKER",
-        false
-      ),
-      new AgronegocioModel(
-        3,
-        1,
-        EspecieAnimalEnum.bovinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bovinos.nome],
-        "SPOCK, JAMES T. KIRK",
-        true
-      ),
-      new AgronegocioModel(
-        4,
-        1,
-        EspecieAnimalEnum.bubalinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bubalinos.nome],
-        "SPOCK, JAMES T. KIRK",
-        true
-      ),
-      new AgronegocioModel(
-        5,
-        2,
-        EspecieAnimalEnum.bovinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bovinos.nome],
-        "THOR, LOKI",
-        true
-      ),
-      new AgronegocioModel(
-        6,
-        2,
-        EspecieAnimalEnum.bovinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bovinos.nome],
-        "THOR, LOKI, ODIN, FRIGGA",
-        true
-      ),
-      new AgronegocioModel(
-        7,
-        2,
-        EspecieAnimalEnum.abelhas.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.abelhas.nome],
-        "LOKI, HELA",
-        true
-      ),
-      new AgronegocioModel(
-        8,
-        3,
-        EspecieAnimalEnum.bovinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bovinos.nome],
-        "BATMAN, BRUCE, ALFRED",
-        true
-      ),
-      new AgronegocioModel(
-        9,
-        3,
-        EspecieAnimalEnum.abelhas.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.abelhas.nome],
-        "LUCIUS FOX, BARBARA GORDON, JAMES W. GORDON",
-        true
-      ),
-      new AgronegocioModel(
-        10,
-        3,
-        EspecieAnimalEnum.bubalinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bubalinos.nome],
-        "BATMAN, ROBIN",
-        true
-      ),
-      new AgronegocioModel(
-        11,
-        3,
-        EspecieAnimalEnum.caprinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.caprinos.nome],
-        "DUAS CARAS, CHARADA",
-        true
-      ),
-      new AgronegocioModel(
-        12,
-        3,
-        EspecieAnimalEnum.aves.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.aves.nome],
-        "MULHER-GATO, PINGUIM, ESPANTALHO",
-        true
-      ),
-      new AgronegocioModel(
-        13,
-        3,
-        EspecieAnimalEnum.abelhas.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.abelhas.nome],
-        "CORINGA, HARLEY QUINN",
-        true
-      ),
-      new AgronegocioModel(
-        14,
-        3,
-        EspecieAnimalEnum.bubalinos.id,
-        EspecieAnimalEnum[EspecieAnimalEnum.bubalinos.nome],
-        "RAS AL GHUL, TALIA AL GHUL",
-        true
-      ),
+    return[
+       {
+        id:0,
+        idGrupoProdutor:0,
+        nomeGrupoProdutor: "LUKE SKYWALKER, ANAKIN SKYWALKER",
+        especieAnimal: EspecieAnimalEnum.bovinos,
+        propriedadeRural: this.propriedadesRurais[0],
+        status: true
+      },
+      {
+        id: 1,
+        idGrupoProdutor: 0,
+        nomeGrupoProdutor: "LUKE SKYWALKER",
+        especieAnimal: EspecieAnimalEnum.bubalinos,
+        propriedadeRural:  this.propriedadesRurais[0],
+        status: true
+      },
+      {
+        id: 2,
+        idGrupoProdutor: 0,
+        nomeGrupoProdutor: "LUKE SKYWALKER, PRINCESS LEIA ORGANA SKYWALKER",
+        especieAnimal: EspecieAnimalEnum.caprinos,
+        propriedadeRural: this.propriedadesRurais[0],
+        status: false
+      },
+      {
+        id: 3,
+        idGrupoProdutor: 1,
+        nomeGrupoProdutor: "SPOCK, JAMES T. KIRK",
+        especieAnimal: EspecieAnimalEnum.bovinos,
+        propriedadeRural: this.propriedadesRurais[1],
+        status: true
+      },
+      {
+        id: 4,
+        idGrupoProdutor: 1,
+        nomeGrupoProdutor: "SPOCK, JAMES T. KIRK",
+        especieAnimal: EspecieAnimalEnum.bubalinos,
+        propriedadeRural: this.propriedadesRurais[1],
+        status: true
+      },
+      {
+        id: 5,
+        idGrupoProdutor: 2,
+        nomeGrupoProdutor: "THOR, LOKI",
+        especieAnimal: EspecieAnimalEnum.bovinos,
+        propriedadeRural: this.propriedadesRurais[2],
+        status: true
+      },
+      {
+        id: 6,
+        idGrupoProdutor: 2,
+        nomeGrupoProdutor: "THOR, LOKI, ODIN, FRIGGA",
+        especieAnimal: EspecieAnimalEnum.bovinos,
+        propriedadeRural: this.propriedadesRurais[2],
+        status: true
+      },
+      {
+        id: 7,
+        idGrupoProdutor: 2,
+        nomeGrupoProdutor: "LOKI, HELA",
+        especieAnimal: EspecieAnimalEnum.abelhas,
+        propriedadeRural: this.propriedadesRurais[2],
+        status: true
+      },
+      {
+        id: 8,
+        idGrupoProdutor: 3,
+        nomeGrupoProdutor: "BATMAN, BRUCE, ALFRED",
+        especieAnimal: EspecieAnimalEnum.bovinos,
+        propriedadeRural: this.propriedadesRurais[3],
+        status:true
+      },
+      {
+        id: 9,
+        idGrupoProdutor: 3,
+        nomeGrupoProdutor: "LUCIUS FOX, BARBARA GORDON, JAMES W. GORDON",
+        especieAnimal: EspecieAnimalEnum.abelhas,
+        propriedadeRural: this.propriedadesRurais[3],
+        status:true
+      },
+      {
+        id: 10,
+        idGrupoProdutor: 3,
+        nomeGrupoProdutor: "BATMAN, ROBIN",
+        especieAnimal: EspecieAnimalEnum.bubalinos,
+        propriedadeRural: this.propriedadesRurais[3],
+        status:true
+      },
+      {
+        id: 11,
+        idGrupoProdutor: 3,
+        nomeGrupoProdutor: "DUAS CARAS, CHARADA",
+        especieAnimal:EspecieAnimalEnum.caprinos,
+        propriedadeRural: this.propriedadesRurais[3],
+        status:true
+      },
+      {
+        id: 12,
+        idGrupoProdutor: 3,
+        nomeGrupoProdutor: "MULHER-GATO, PINGUIM, ESPANTALHO",
+        especieAnimal: EspecieAnimalEnum.aves,
+        propriedadeRural: this.propriedadesRurais[3],
+        status:true
+      },
+      {
+        id: 13,
+        idGrupoProdutor: 3,
+        nomeGrupoProdutor: "CORINGA, HARLEY QUINN",
+        especieAnimal: EspecieAnimalEnum.abelhas,
+        propriedadeRural: this.propriedadesRurais[3],
+        status:true
+      },
+      {
+        id:14,
+        idGrupoProdutor:3,
+        nomeGrupoProdutor: "RAS AL GHUL, TALIA AL GHUL",
+        especieAnimal: EspecieAnimalEnum.bovinos,
+        propriedadeRural: this.propriedadesRurais[3],
+        status:true
+      }
     ];
   }
 
@@ -186,7 +206,9 @@ export class PopulaDados {
         id: 0,
         idAgronegocio: 0,
         dtLancamento: "10/01/2020",
-        tipoLancamento: 0,
+        tipoLancamento: this.tiposLancamento[0],
+        blnEnviado:true,
+        dtEnvio: '11/01/2020',
         subAgrupamento: [
           {
             idAgrupamento: CategoriasEnum.ate_12_meses.id,
@@ -204,7 +226,8 @@ export class PopulaDados {
         id: 3,
         idAgronegocio: 1,
         dtLancamento: "14/01/2020",
-        tipoLancamento: 1,
+        tipoLancamento: this.tiposLancamento[1],
+        blnEnviado:false,
         subAgrupamento: [
           {
             idAgrupamento: CategoriasEnum.de_13_a_24_meses.id,
@@ -215,9 +238,9 @@ export class PopulaDados {
             idAgrupamento: CategoriasEnum.de_13_a_24_meses.id,
             idSubAgrupamento: SubCategoriaEnum.macho.id,
             quantidade: 2,
-          },
-        ],
-      },
+          }
+        ]
+      }
     ];
   }
 
@@ -351,5 +374,20 @@ export class PopulaDados {
         ]
       ),
     ];
+  }
+  populaTiposLancamentos(){
+    return [
+      new TiposLancamentosModel(
+        0,
+        "Evolução Rebanho",
+        false
+      ),
+      new TiposLancamentosModel(
+        1,
+        "Sem Movimentação",
+        false
+      )
+    ]
+    
   }
 }
